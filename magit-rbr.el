af0edcb67e1668c76c22b649e3a4a3168747c503
Anatoly Fayngelerin <afayngelerin@dropbox.com>
2018-07-18 17:00:49 -0400

    make sure readme is consistent with package commentary

--- magit-rbr.el
+++ magit-rbr.el
@@ -16,10 +16,11 @@
 ;;; Commentary:
 ;;;
 ;;; This package tweaks magit to recognize `git rbr` rebases and use
-;;; corresponding commands during the magit rebase sequence.  This
+;;; corresponding commands during the magit rebase sequence. This
 ;;; means that when you abort a rebase during a recursive rebase,
 ;;; magit will abort the rbr rather than a particular phase of
-;;; rbr.  This also adds recursive rebase as an option
+;;; rbr. This also adds recursive rebase as an option to the rebase
+;;; popup.
 ;;;
 ;;; Code:
 (require 'magit)
