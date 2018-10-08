2384f8d660b0dab2a802bd0faaf8099ce20ca340
Jonas Bernoulli <jonas@bernoul.li>
2018-10-09 01:50:16 +0200

    Use correct amount of semicolons for commentary
    Comments that begin with three or more semicolons are
    considered headings by `outline-minor-mode' and friends.

--- magit-rbr.el
+++ magit-rbr.el
@@ -14,15 +14,16 @@
 ;; This file is free software...
 
 ;;; Commentary:
-;;;
-;;; This package tweaks magit to recognize `git rbr` rebases and use
-;;; corresponding commands during the magit rebase sequence. This
-;;; means that when you abort a rebase during a recursive rebase,
-;;; magit will abort the rbr rather than a particular phase of
-;;; rbr. This also adds recursive rebase as an option to the rebase
-;;; popup.
-;;;
+
+;; This package tweaks magit to recognize `git rbr` rebases and use
+;; corresponding commands during the magit rebase sequence. This
+;; means that when you abort a rebase during a recursive rebase,
+;; magit will abort the rbr rather than a particular phase of
+;; rbr. This also adds recursive rebase as an option to the rebase
+;; popup.
+
 ;;; Code:
+
 (require 'magit)
 (require 'magit-sequence)
 
