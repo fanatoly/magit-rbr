8709b973e3a65e7478ad7dcbb503fd39f4be9ae1
Jonas Bernoulli <jonas@bernoul.li>
2018-10-09 02:25:25 +0200

    Remove unnecessary code and fix fatal bug (#1)
    * Use correct amount of semicolons for commentary
    
    Comments that begin with three or more semicolons are
    considered headings by `outline-minor-mode' and friends.
    
    * Shorten long line
    
    * Use more conventional messages
    
    * Remove redefinitions of magit commands that are no longer necessary
    
    * magit-rbr-rebase-recursive: Fix infinite recursion
    
    This should be done by fixing the name of the called
    function, but I went with inlining that instead.

--- magit-rbr.el
+++ magit-rbr.el
@@ -14,74 +14,29 @@
 ;; This file is free software...
 
 ;;; Commentary:
-;;;
-;;; This package tweaks magit to recognize `git rbr` rebases and use
-;;; corresponding commands during the magit rebase sequence. This
-;;; means that when you abort a rebase during a recursive rebase,
-;;; magit will abort the rbr rather than a particular phase of
-;;; rbr. This also adds recursive rebase as an option to the rebase
-;;; popup.
-;;;
+
+;; This package tweaks magit to recognize `git rbr` rebases and use
+;; corresponding commands during the magit rebase sequence. This
+;; means that when you abort a rebase during a recursive rebase,
+;; magit will abort the rbr rather than a particular phase of
+;; rbr. This also adds recursive rebase as an option to the rebase
+;; popup.
+
 ;;; Code:
+
 (require 'magit)
 (require 'magit-sequence)
-
-(defun magit-rebase-command ()
-  (if (file-exists-p (magit-git-dir "rebase-recursive")) "rbr" "rebase"))
-
-(defun magit-rebase-continue (&optional noedit)
-  "Restart the current rebasing operation.
-In some cases this pops up a commit message buffer for you do
-edit.  With a prefix argument the old message is reused as-is."
-  (interactive "P")
-  (if (magit-rebase-in-progress-p)
-      (if (magit-anything-unstaged-p t)
-          (user-error "Cannot continue rebase with unstaged changes")
-        (when (and (magit-anything-staged-p)
-                   (file-exists-p (magit-git-dir "rebase-merge"))
-                   (not (member (magit-toplevel)
-                                magit--rebase-public-edit-confirmed)))
-          (magit-commit-amend-assert))
-        (if noedit
-            (let ((process-environment process-environment))
-              (push "GIT_EDITOR=true" process-environment)
-              (magit-run-git-async (magit-rebase-command) "--continue")
-              (set-process-sentinel magit-this-process
-                                    #'magit-sequencer-process-sentinel)
-              magit-this-process)
-          (magit-run-git-sequencer (magit-rebase-command) "--continue")))
-    (user-error "No rebase in progress")))
-
-(defun magit-rebase-skip ()
-  "Skip the current commit and restart the current rebase operation."
-  (interactive)
-  (unless (magit-rebase-in-progress-p)
-    (user-error "No rebase in progress"))
-  (magit-run-git-sequencer (magit-rebase-command) "--skip"))
-
-
-(defun magit-rebase-abort ()
-  "Abort the current rebase operation, restoring the original branch."
-  (interactive)
-  (unless (magit-rebase-in-progress-p)
-    (user-error "No rebase in progress"))
-  (magit-confirm 'abort-rebase "Abort this rebase")
-  (magit-run-git (magit-rebase-command) "--abort"))
-
 
 ;;;###autoload
 (defun magit-rbr-rebase-recursive (args)
   "Rebase the current branch recursively onto its upstream."
   (interactive (list (magit-rebase-arguments)))
-  (message "Recursively Rebasing...")
-  (magit-git-rebase-recursive args)
-  (message "Recursively Rebasing...done"))
+  (message "Rebasing recursively ...")
+  (magit-run-git-sequencer "rbr" args)
+  (message "Rebasing recursively...done"))
 
-;;;###autoload
-(defun magit-rbr-git-rebase-recursive (args)
-  (magit-run-git-sequencer "rbr" args))
-
-(magit-define-popup-action 'magit-rebase-popup ?r "recursively" 'magit-rbr-rebase-recursive ?i t)
+(magit-define-popup-action 'magit-rebase-popup
+  ?r "recursively" 'magit-rbr-rebase-recursive ?i t)
 
 (provide 'magit-rbr)
 
