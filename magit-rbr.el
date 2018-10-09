aaf8bfdaae974805729a859bb05ebd63743127e5
Anatoly Fayngelerin <afayngelerin@dropbox.com>
2018-10-09 06:53:41 -0400

    Applied feedback from @tarsius. Removed Created tag. Removed version tag. Fixed copyright dates(I think). Added GPL licence

--- magit-rbr.el
+++ magit-rbr.el
@@ -1,17 +1,27 @@
 ;;; magit-rbr.el --- Support for git rbr in Magit
 
-;; Copyright (C) 2010-2018 Anatoly Fayngelerin
+;; Copyright (C) 2018 Anatoly Fayngelerin
 
 ;; Author: Anatoly Fayngelerin <fanatoly+magitrbr@gmail.com>
 ;; Maintainer: Anatoly Fayngelerin <fanatoly+magitrbr@gmail.com>
-;; Created: 18 Jul 2018
 ;; Version: 20180718.00
 ;; Keywords: git magit rbr tools
 ;; Homepage: https://github.com/fanatoly/magit-rbr
 ;; Package-Requires: ((magit "2.13.0") (emacs "24.3"))
 ;; This file is not part of GNU Emacs.
 
-;; This file is free software...
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
